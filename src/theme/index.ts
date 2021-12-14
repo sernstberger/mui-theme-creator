fe6952211018a0c1a8fe1b38f1fae0c80a011138
Steve Ernstberger <steve32285@gmail.com>
2021-12-13 21:02:59 -0700

    using createTheme instead of object assign

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -4,12 +4,13 @@ import { alpha } from "@mui/material";
 
 const purple = "#7F56D9";
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#FF9999',
-    },
-  },
-});
+// const theme = (what: any) => createTheme({
+//   palette: {
+//     primary: {
+//       main: what.primaryColor,
+//     },
+//   },
+// });
+const theme = 'foo'
 
 export default theme;
