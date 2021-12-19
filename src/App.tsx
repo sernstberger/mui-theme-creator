7c1779975a65dea2042a63444cd34f43e87addb2
Steve Ernstberger <steve32285@gmail.com>
2021-12-19 12:31:09 -0500

    app scrolling is better

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import {
   Stack,
   Divider,
   TextField,
+  Container,
+  Box,
 } from "@mui/material";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
@@ -19,127 +21,139 @@ import ColorSwatch from "./components/ColorSwatch";
 const App = () => {
   const { values } = useFormikContext<any>();
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={3}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Sidebar />
-        </ThemeProvider>
+    <Box overflow="hidden" height="100vh">
+      <Grid container spacing={2}>
+        <Grid item xs={3}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Sidebar />
+          </ThemeProvider>
+        </Grid>
+        <Grid item xs={9}>
+          <Box height="100vh" overflow="scroll">
+            <ThemeProvider theme={dynamicTheme()}>
+              <CssBaseline />
+              <>
+                <MuiTypography variant="h1">Content goes here</MuiTypography>
+                <MuiTypography variant="h2">Content goes here</MuiTypography>
+                <MuiTypography variant="h3">Content goes here</MuiTypography>
+                <MuiTypography variant="h4">Content goes here</MuiTypography>
+                <MuiTypography variant="h5">Content goes here</MuiTypography>
+                <MuiTypography variant="h6">Content goes here</MuiTypography>
+                <MuiTypography variant="subtitle1">
+                  Content goes here
+                </MuiTypography>
+                <MuiTypography variant="subtitle2">
+                  Content goes here
+                </MuiTypography>
+                <MuiTypography variant="body1">Content goes here</MuiTypography>
+                <MuiTypography variant="body2">Content goes here</MuiTypography>
+                <MuiTypography variant="caption">
+                  Content goes here
+                </MuiTypography>
+                <MuiTypography variant="button">
+                  Content goes here
+                </MuiTypography>
+                <MuiTypography variant="overline">
+                  Content goes here
+                </MuiTypography>
+                <MuiButton variant={values.button.variant}>Woot</MuiButton>
+                <Stack spacing={2} direction="row">
+                  <MuiButton variant="contained">Button CTA</MuiButton>
+                  <MuiButton variant="contained" className="Mui-hover">
+                    Button CTA
+                  </MuiButton>
+                  <MuiButton variant="contained" className="Mui-focusVisible">
+                    Button CTA
+                  </MuiButton>
+                  <MuiButton variant="contained" disabled>
+                    Button CTA
+                  </MuiButton>
+                </Stack>
+                <Stack spacing={2} direction="row">
+                  <MuiButton color="secondary" variant="contained">
+                    Button CTA
+                  </MuiButton>
+                  <MuiButton
+                    color="secondary"
+                    variant="contained"
+                    className="Mui-hover"
+                  >
+                    Button CTA
+                  </MuiButton>
+                  <MuiButton
+                    color="secondary"
+                    variant="contained"
+                    className="Mui-focusVisible"
+                  >
+                    Button CTA
+                  </MuiButton>
+                  <MuiButton color="secondary" variant="contained" disabled>
+                    Button CTA
+                  </MuiButton>
+                </Stack>
+                <br />
+                <Divider />
+                <br />
+                <div>
+                  <TextField label="Foooooo" />
+                </div>
+                <br />
+                <Divider />
+                <br />
+                {/* TODO - make these color swatch code blocks dynamic */}
+                primary
+                <Stack direction="row">
+                  <ColorSwatch color={values.palette.primary.light} />
+                  <ColorSwatch color={values.palette.primary.main} />
+                  <ColorSwatch color={values.palette.primary.dark} />
+                  <ColorSwatch color={values.palette.primary.contrastText} />
+                </Stack>
+                secondary
+                <Stack direction="row">
+                  <ColorSwatch color={values.palette.secondary.light} />
+                  <ColorSwatch color={values.palette.secondary.main} />
+                  <ColorSwatch color={values.palette.secondary.dark} />
+                  <ColorSwatch color={values.palette.secondary.contrastText} />
+                </Stack>
+                error
+                <Stack direction="row">
+                  <ColorSwatch color={values.palette.error.light} />
+                  <ColorSwatch color={values.palette.error.main} />
+                  <ColorSwatch color={values.palette.error.dark} />
+                  <ColorSwatch color={values.palette.error.contrastText} />
+                </Stack>
+                warning
+                <Stack direction="row">
+                  <ColorSwatch color={values.palette.warning.light} />
+                  <ColorSwatch color={values.palette.warning.main} />
+                  <ColorSwatch color={values.palette.warning.dark} />
+                  <ColorSwatch color={values.palette.warning.contrastText} />
+                </Stack>
+                success
+                <Stack direction="row">
+                  <ColorSwatch color={values.palette.success.light} />
+                  <ColorSwatch color={values.palette.success.main} />
+                  <ColorSwatch color={values.palette.success.dark} />
+                  <ColorSwatch color={values.palette.success.contrastText} />
+                </Stack>
+                <br />
+                <Divider />
+                <br />
+                <SyntaxHighlighter
+                  language="typescript"
+                  style={dark}
+                  wrapLines={true}
+                >
+                  {JSON.stringify(dynamicTheme())}
+                </SyntaxHighlighter>
+                {JSON.stringify(dynamicTheme())}
+              </>
+            </ThemeProvider>
+          </Box>
+        </Grid>
       </Grid>
-      <Grid item xs={9}>
-
-
-        <ThemeProvider theme={dynamicTheme()}>
-          <CssBaseline />
-          <>
-            <MuiTypography variant="h1">Content goes here</MuiTypography>
-            <MuiTypography variant="h2">Content goes here</MuiTypography>
-            <MuiTypography variant="h3">Content goes here</MuiTypography>
-            <MuiTypography variant="h4">Content goes here</MuiTypography>
-            <MuiTypography variant="h5">Content goes here</MuiTypography>
-            <MuiTypography variant="h6">Content goes here</MuiTypography>
-            <MuiTypography variant="subtitle1">Content goes here</MuiTypography>
-            <MuiTypography variant="subtitle2">Content goes here</MuiTypography>
-            <MuiTypography variant="body1">Content goes here</MuiTypography>
-            <MuiTypography variant="body2">Content goes here</MuiTypography>
-            <MuiTypography variant="caption">Content goes here</MuiTypography>
-            <MuiTypography variant="button">Content goes here</MuiTypography>
-            <MuiTypography variant="overline">Content goes here</MuiTypography>
-            <MuiButton variant={values.button.variant}>Woot</MuiButton>
-            <Stack spacing={2} direction="row">
-              <MuiButton variant="contained">Button CTA</MuiButton>
-              <MuiButton variant="contained" className="Mui-hover">
-                Button CTA
-              </MuiButton>
-              <MuiButton variant="contained" className="Mui-focusVisible">
-                Button CTA
-              </MuiButton>
-              <MuiButton variant="contained" disabled>
-                Button CTA
-              </MuiButton>
-            </Stack>
-            <Stack spacing={2} direction="row">
-              <MuiButton color="secondary" variant="contained">
-                Button CTA
-              </MuiButton>
-              <MuiButton
-                color="secondary"
-                variant="contained"
-                className="Mui-hover"
-              >
-                Button CTA
-              </MuiButton>
-              <MuiButton
-                color="secondary"
-                variant="contained"
-                className="Mui-focusVisible"
-              >
-                Button CTA
-              </MuiButton>
-              <MuiButton color="secondary" variant="contained" disabled>
-                Button CTA
-              </MuiButton>
-            </Stack>
-            <br />
-            <Divider />
-            <br />
-            <div>
-              <TextField label="Foooooo" />
-            </div>
-            <br />
-            <Divider />
-            <br />
-            {/* TODO - make these color swatch code blocks dynamic */}
-            primary
-            <Stack direction="row">
-              <ColorSwatch color={values.palette.primary.light} />
-              <ColorSwatch color={values.palette.primary.main} />
-              <ColorSwatch color={values.palette.primary.dark} />
-              <ColorSwatch color={values.palette.primary.contrastText} />
-            </Stack>
-            secondary
-            <Stack direction="row">
-              <ColorSwatch color={values.palette.secondary.light} />
-              <ColorSwatch color={values.palette.secondary.main} />
-              <ColorSwatch color={values.palette.secondary.dark} />
-              <ColorSwatch color={values.palette.secondary.contrastText} />
-            </Stack>
-            error
-            <Stack direction="row">
-              <ColorSwatch color={values.palette.error.light} />
-              <ColorSwatch color={values.palette.error.main} />
-              <ColorSwatch color={values.palette.error.dark} />
-              <ColorSwatch color={values.palette.error.contrastText} />
-            </Stack>
-            warning
-            <Stack direction="row">
-              <ColorSwatch color={values.palette.warning.light} />
-              <ColorSwatch color={values.palette.warning.main} />
-              <ColorSwatch color={values.palette.warning.dark} />
-              <ColorSwatch color={values.palette.warning.contrastText} />
-            </Stack>
-            success
-            <Stack direction="row">
-              <ColorSwatch color={values.palette.success.light} />
-              <ColorSwatch color={values.palette.success.main} />
-              <ColorSwatch color={values.palette.success.dark} />
-              <ColorSwatch color={values.palette.success.contrastText} />
-            </Stack>
-            <br />
-            <Divider />
-            <br />
-            <SyntaxHighlighter
-              language="typescript"
-              style={dark}
-              wrapLines={true}
-            >
-              {JSON.stringify(dynamicTheme())}
-            </SyntaxHighlighter>
-            {JSON.stringify(dynamicTheme())}
-          </>
-        </ThemeProvider>
-      </Grid>
-    </Grid>
+    </Box>
   );
 };
 
