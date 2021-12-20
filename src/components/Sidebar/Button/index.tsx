0bb75ce04677a834a6e9327aed55426cd9f2166c
Steve Ernstberger <steve32285@gmail.com>
2021-12-19 22:04:07 -0500

    added padding component

--- src/components/Sidebar/Button/index.tsx
+++ src/components/Sidebar/Button/index.tsx
@@ -2,15 +2,10 @@ import { Box, Divider, Stack, Typography } from "@mui/material";
 import SidebarItem from "../SidebarItem";
 import SelectInput from "../../Form/SelectInput";
 import Switch from "../../Form/Switch";
-import { useFormikContext } from "formik";
 import TextInput from "../../Form/TextInput";
+import Padding from "../../CssProperties/Padding";
 
 const Button = () => {
-  const { values, setFieldValue } = useFormikContext<any>();
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFieldValue("buttonDefaultProps.disabled", event.target.checked);
-  };
   return (
     <SidebarItem title="Button">
       <Typography variant="subtitle2">Prop defaults</Typography>
@@ -45,28 +40,7 @@ const Button = () => {
           <Switch field="buttonDefaultProps.fullWidth" label="Full width" />
           <Divider />
           <Typography variant="subtitle2">Style defaults</Typography>
-          <Stack direction="row" spacing={1}>
-            <TextInput
-              field="buttonStyle.styleOverrides.padding[0]"
-              label="Padding"
-              type="number"
-            />
-            <TextInput
-              field="buttonStyle.styleOverrides.padding[1]"
-              label="Padding"
-              type="number"
-            />
-            <TextInput
-              field="buttonStyle.styleOverrides.padding[2]"
-              label="Padding"
-              type="number"
-            />
-            <TextInput
-              field="buttonStyle.styleOverrides.padding[3]"
-              label="Padding"
-              type="number"
-            />
-          </Stack>
+          <Padding field="buttonStyle.styleOverrides.padding" />
         </Stack>
       </Box>
     </SidebarItem>
