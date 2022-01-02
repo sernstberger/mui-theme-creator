445a4505dd6e7fba7f180de8da9422b3b43cfdc6
Steve Ernstberger <steve32285@gmail.com>
2022-01-02 09:55:40 -0500

    color changes now

--- src/components/Sidebar/Palette/Color.tsx
+++ src/components/Sidebar/Palette/Color.tsx
@@ -1,24 +1,28 @@
-import { useEffect } from "react";
-import { useFormikContext, getIn } from "formik";
 import { Stack } from "@mui/material";
 import ColorSwatch from "../../ColorSwatch";
 import { createPaletteColor } from "../../../utils";
 import ValidatedTextInput from "../../Form/ValidatedTextInput";
+import { useFormContext, useWatch } from "react-hook-form";
 
 const Color = ({ field, label }: any) => {
-  const { values, handleChange, touched, setFieldValue } =
-    useFormikContext<any>();
+  // const { values, handleChange, touched, setFieldValue } =
+  //   useFormikContext<any>();
 
-  useEffect(() => {
-    const getColorString = (meh: string) =>
-      getIn(values, `palette.${field}.${meh}`);
-    const colorPaletteObj = createPaletteColor(getColorString("main"));
-    setFieldValue(`palette.${field}`, colorPaletteObj);
-  }, [values.palette[field].main]);
+  // useEffect(() => {
+  //   const getColorString = (meh: string) =>
+  //     getIn(values, `palette.${field}.${meh}`);
+  //   const colorPaletteObj = createPaletteColor(getColorString("main"));
+  //   setFieldValue(`palette.${field}`, colorPaletteObj);
+  // }, [values.palette[field].main]);
+
+  const { getValues } = useFormContext();
+  const foo = getValues();
+
+  useWatch({ name: `palette.${field}.main` });
 
   return (
     <Stack direction="row" spacing={2}>
-      <ColorSwatch color={getIn(values, `palette.${field}.main`)} />
+      <ColorSwatch color={foo.palette[field].main} />
       <ValidatedTextInput fieldName={`palette.${field}.main`} {...{ label }} />
     </Stack>
   );
